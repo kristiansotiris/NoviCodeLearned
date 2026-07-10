d31721e88794f4858fa2885dfca89910770f4b8c
christian <User@DESKTOP-FS5J75R>
2026-07-10 15:05:42 +0300

    initialized changes updated "application"

---
+++ Application/Interfaces/IBetRepository.cs
@@ -0,0 +1,14 @@
+﻿using NoviBet.Domain.Entities;
+using NoviBet.Domain.Enums;
+
+namespace Application.Interfaces
+{
+    public interface IBetRepository
+    {
+        void CreateBet(Bet bet);
+        IReadOnlyList<Bet> GetPlayerBets(Guid playerId);
+        IReadOnlyList<Bet> GetBetsByStatus(BetStatus betStatus);
+        IReadOnlyList<Bet> GetAllBets();
+
+    }
+}
---
+++ Application/Interfaces/IMatchRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Application.Interfaces
+{
+    public interface IMatchRepository
+    {
+        void AddMatch(Match match);
+        Match? GetMatchById(Guid Id);
+        IReadOnlyList<Match> GetAllMatches();
+
+    }
+}
